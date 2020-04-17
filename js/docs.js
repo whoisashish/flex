be90f2d75303c0b10953f08f0644e13e066e2140
ASHISH YADAV <nowwhoisashish@gmail.com>
2020-04-17 23:41:18 +0530

    Create docs.js

---
+++ js/docs.js
@@ -0,0 +1,33 @@
+var docs = {
+  'align-content': {
+    'en': '<p>Aligns a flex container\'s lines within the flex container when there is extra space on the cross-axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch (default)</code>',
+ 'hi': '<p>यह कंटेनर के भीतर रखी वस्तुओं के मध्य की जगह को संरेखित करता है।</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch (default)</code>'
+ },
+  'align-items': {
+    'en': '<p>Aligns flex items along the cross axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch (default)</code>',
+ 'hi': '<p>वस्तुओं को उर्ध्वाधर रूप से संरेखित करता है।</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch (default)</code>'
+},
+  'align-self': {
+    'en': '<p>Aligns a flex item along the cross axis, overriding the <code>align-items</code> value.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
+ 'hi': '<p>छैतिज रूप से किसी विशिष्ट वस्तु के लिए नियम प्रदान करता है, <code>align-items</code> के नियमों को न मानते हुए।</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>'
+},
+  'flex-direction': {
+    'en': '<p>Defines the direction of the main axis.</p><code>row (default)</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
+ 'hi': '<p>मुख्य धुरी की दिशा परिभाषित करता है।</p><code>row (default)</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>'
+ },
+  'flex-flow': {
+    'en': '<p>Shorthand property for <code>flex-direction</code> and <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>',
+  'hi': '<p>शोर्ट हैंड प्रॉपर्टी <code>flex-direction</code> एवं <code>flex-wrap</code> के लिए।</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>'
+},
+  'flex-wrap': {
+    'en': '<p>Specifies whether flex items are forced on a single line or can be wrapped on multiple lines.</p><code>nowrap (default)</code> <code>wrap</code> <code>wrap-reverse</code>',
+    'hi': '<p>यह निर्दिष्ठ करता है की कोई वस्तु एक पंक्ति अथवा कई पंक्तियों में आएगी।</p><code>nowrap (default)</code> <code>wrap</code> <code>wrap-reverse</code>'
+ },
+  'justify-content': {
+    'en': '<p>Aligns flex items along the main axis.</p><code>flex-start (default)</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
+  'hi': '<p>वस्तुओं को छैतिज रूप से संरेखित करता है।</p><code>flex-start (default)</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>'
+ },
+  'order': {
+    'en': '<p>Specifies the order of the flex item.</p><code>&lt;integer&gt; (... -1, 0 (default), 1, ...)</code>',
+    'hi': '<p>फ्लेक्स आइटम का क्रम निश्चित करता है।</p><code>&lt;पूर्णांक&gt; (... -1, 0 (default), 1, ...)</code>'  }
+};
